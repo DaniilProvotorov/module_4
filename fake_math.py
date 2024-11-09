efc903aafb29fbe2d11eb541a6130109532152b2
DaniilProvotorov <dantcsar@gmail.com>
2024-11-09 19:32:45 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+
+def divide(firs, second):
+    if second != 0:
+        return firs/second
+    else: return 'Ошибка!'
+
---
+++ module_4_1.py
@@ -0,0 +1,7 @@
+from fake_math import divide as div_f
+from true_math import divide as div_t
+
+print(div_f(32, 2))
+print(div_f(61, 0))
+print(div_t(45, 5))
+print(div_t(69, 0))
